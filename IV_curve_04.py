31bfedaad5b868ff4a0a49255df357579d7a8b62
rawrjagwar <86827965+rawrjagwar@users.noreply.github.com>
2024-05-15 15:39:26 +0200

    new file for switch system tests

---
+++ IV_curve_04.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 18:35:54 2024
+
+@author: coray
+
+Script to create an IV-Curve for one cell, according to the parameters
+provided by ASCA.
+
+Runs one cell through the 7001 Switch System. Closing two sets of channels to 
+measure and then opening them again after completion
+
+"""
+
+# Packages
+import pyvisa
+import pymeasure
+import numpy as np
+import pandas as pd
+from time import sleep
+from pymeasure.adapters import PrologixAdapter
+from pymeasure.instruments.keithley import Keithley2400
+from pymeasure.instruments.keithley import Keithley2750
+
+# Instruments
+adapter = PrologixAdapter('ASRL3::INSTR')
+sourcemeter = Keithley2400(adapter.gpib(24))  # at GPIB address 24
+switchsystem = Keithley2750(adapter.gpib(17))  # at GPIB address 17
+
+# Variables
+data_points = 100
+averages = 10
+max_voltage = 0 # in Volts
+min_voltage = -25 # in Volts
+
+# Switch System Setup
+cells = 1
+input_channels = [] # pins 13a and 15a
+output_channels = [] # pins 14a and 16a
+
+# Parameters
+voltage_range = 25 # in Volts
+compliance_current = 25e-03  # in Amps
+measure_nplc = 0.1  # Number of power line cycles
+current_range = 25e-03  # in Amps
+
+# Configure the Sourcemeter
+sourcemeter.reset()
+sourcemeter.use_front_terminals()
+sourcemeter.apply_voltage(voltage_range, compliance_current)
+sourcemeter.measure_current(measure_nplc)
+sleep(0.1)  # wait here to give the instrument time to react
+sourcemeter.stop_buffer()
+sourcemeter.disable_buffer()
+
+# Create Arrays for Results
+voltages = np.linspace(max_voltage, min_voltage, num=data_points)
+currents = np.zeros_like(voltages)
+current_stds = np.zeros_like(voltages)
+
+# Activate Output of Sourcemeter
+sourcemeter.enable_source()
+
+# Loop through each current point, measure and record the voltage
+for i in range(data_points):
+    sourcemeter.config_buffer(averages)
+    sourcemeter.source_voltage = voltages[i]
+    sourcemeter.start_buffer()
+    sourcemeter.wait_for_buffer()
+    # Record the average and standard deviation
+    currents[i] = sourcemeter.mean_current
+    sleep(0.01)
+    current_stds[i] = sourcemeter.standard_devs[1]
+
+# Save the data columns in a CSV file
+data = pd.DataFrame({
+    'Voltage (V)': voltages,
+    'Current (A)': currents,
+    'Current Std (A)': current_stds,
+})
+data.to_csv('example_02.csv')
+
+sourcemeter.shutdown()
